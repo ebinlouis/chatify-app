78446e6b538da476275695ce306799934b0f6356
Ebin Louis <ebinlouis1559@gmail.com>
2025-10-03 12:46:56 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import { fileURLToPath } from 'url';
 
 dotenv.config({ quiet: true });
 
@@ -9,9 +11,19 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, '../../frontend', 'dist', 'index.html'));
+    });
+}
 app.listen(PORT, () => {
     console.log(`Server Running on port ${PORT}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-    return <div>Hello World</div>;
+    return <div>Hello World Heyy</div>;
 };
 
 export default App;
