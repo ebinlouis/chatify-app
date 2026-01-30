aca432090946b7f3867c4325154a5cc2522ba2f7
Ebin Louis <ebinlouis1559@gmail.com>
2026-01-30 06:29:54 +0530

    added the socket io and the add the getUsersOnline also

---
+++ backend/src/lib/socket.js
@@ -0,0 +1,33 @@
+import { Server } from 'socket.io';
+import http from 'http';
+import express from 'express';
+import { ENV } from './../lib/env.js';
+import { socketAuthMiddleware } from '../middleware/socket.middleware.js';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: ENV.CLIENT_URL,
+        credentials: true,
+    },
+});
+
+io.use(socketAuthMiddleware);
+
+const userSocketMap = {};
+
+io.on('connection', (socket) => {
+    console.log(`A User Connected : ${socket.user.fullName}`);
+
+    userSocketMap[socket.userId] = socket.id;
+    io.emit('getOnlineUsers', Object.keys(userSocketMap));
+
+    socket.on('disconnect', () => {
+        console.log('A User disconnected', socket.user.fullName);
+        delete userSocketMap[socket.userId];
+        io.emit('getOnlineUsers', Object.keys(userSocketMap));
+    });
+});
+
+export { io, app, server };
